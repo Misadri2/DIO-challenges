143e44afe059ea69a357fa5d9ca7378eed653f43
Your Name <mis.monterio@hotmail.com>
2021-03-06 13:33:10 -0300

    alterado pasta

---
+++ DesafioMesAno/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DesafioDio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, anos, meses, dias;
+
+            Console.Write("Informe valor de n: ");
+            n = int.Parse(Console.ReadLine());
+            anos = n/365;
+            meses = (n % 365)/30;
+            dias = ((n % 365)% 30);
+
+            Console.WriteLine("Resultado: " + anos);
+            Console.WriteLine("Resultado: " + meses);
+            Console.WriteLine("Resultado: " + dias );
+        }
+    }
+}
