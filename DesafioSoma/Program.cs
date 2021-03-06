992eb9f4557d31f5e261b63508f1e4670fedb90e
Your Name <mis.monterio@hotmail.com>
2021-03-06 12:57:39 -0300

    add desafio soma

---
+++ DesafioSoma/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DesafioDio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            x = (a) + (b); //complete o código de acordo com o cálculo esperado
+
+            Console.WriteLine("X = " + x);
+        }
+    }
+}
