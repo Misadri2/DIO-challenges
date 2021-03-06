17b3d69d55e348f4c986d21dbbe7c80f6cc4d59b
Your Name <mis.monterio@hotmail.com>
2021-03-06 13:26:33 -0300

    add desafio tweet

---
+++ DesafioTweet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DesafioTweet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string v = Console.ReadLine();
+            char[] arr = v.ToCharArray();
+            if(  arr.Length <= 140  ){ //complete a condicional
+                Console.WriteLine("TWEET");
+           } else{
+                Console.WriteLine("MUTE");
+           }
+        }
+    }
+}
